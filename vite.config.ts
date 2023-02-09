7ef18fc233624b50eaacee62013f6cede9f8e5d4
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-02-09 10:58:47 +0300

    плагин для сжатия изображений в prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,39 @@
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import viteImagemin from "vite-plugin-imagemin";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-})
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false,
+          },
+        ],
+      },
+    }),
+  ],
+});
