abef89b466e490b145af78c1a22d977f8caff037
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-03-08 14:27:58 +0300

    fix button

--- src/shared/ui/Button/ui/Button.tsx
+++ src/shared/ui/Button/ui/Button.tsx
@@ -1,35 +1,33 @@
-import { FC, SyntheticEvent, ReactNode } from 'react'
+import { FC, SyntheticEvent, ReactNode } from "react";
 
 export interface ButtonProps {
-  children: ReactNode
-  rounded?: 'rounded-md' | 'rounded-lg'
-  onClick?: (e: SyntheticEvent) => void
-  size?: 'sm' | 'md' | 'lg'
-  textColor?: 'white' | 'blue' | string
-  color?: 'primary' | 'light' | string
-  disabled?: boolean
-  className?: string
+  children: ReactNode;
+  onClick?: (e: SyntheticEvent) => void;
+  color?: "primary" | "light" | string;
+  disabled?: boolean;
+  size?: "sm" | "md" | "lg";
+  className?: string;
 }
 
 export const Button: FC<ButtonProps> = ({
   children,
-  rounded,
   onClick,
-  size = 'md',
-  color = 'primary',
-  textColor = 'white',
+  color = "primary",
   disabled = false,
-  className = '',
+  size = "md",
+  className = "",
 }) => {
-  const disabledStyles = disabled ? 'disabled:bg-gray-light disabled:hover:cursor-not-allowed' : ''
+  const disabledStyles = disabled
+    ? "disabled:bg-gray-light disabled:hover:cursor-not-allowed"
+    : "";
 
   return (
     <button
       disabled={disabled}
-      className={`${className} ${size} ${rounded || ''} text-${textColor} bg-${color} 
-      outline-none focus-visible:ring-1 shrink-0 ${disabledStyles}`}
-      onClick={onClick}>
+      className={`base-button ${className} bg-${color} ${size} ${disabledStyles}`}
+      onClick={onClick}
+    >
       {children}
     </button>
-  )
-}
+  );
+};
