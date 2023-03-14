739f8dc02d043c929b129f1183404f45ea84463d
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-03-14 10:00:02 +0300

    правило для фигурных скобок + запрет для обновления eslint plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,51 +4,55 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "airbnb-base",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:promise/recommended",
-    "plugin:react-hooks/recommended",
-    "airbnb-typescript",
-    "plugin:react/jsx-runtime",
-    "plugin:conarti-fsd/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:promise/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb-typescript',
+    'plugin:react/jsx-runtime',
+    'plugin:conarti-fsd/recommended',
+    'prettier',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: "./tsconfig.json",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
         alwaysTryTypes: true,
       },
     },
   },
-  plugins: ["react", "@typescript-eslint", "conarti-fsd", "import", "prettier"],
+  plugins: ['react', '@typescript-eslint', 'conarti-fsd', 'import', 'prettier'],
   rules: {
-    "conarti-fsd/layers-slices": [
-      "error",
+    'conarti-fsd/layers-slices': [
+      'error',
       {
         allowTypeImports: true,
       },
     ],
-    "conarti-fsd/absolute-relative": "error",
-    "conarti-fsd/public-api": "error",
-    "react/react-in-jsx-scope": "off",
-    "react/jsx-uses-react": "off",
-    "react-hooks/exhaustive-deps": "off",
-    "react/jsx-filename-extension": [
+    'conarti-fsd/absolute-relative': 'error',
+    'conarti-fsd/public-api': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'react/jsx-filename-extension': [
       1,
       {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     ],
-    "import/prefer-default-export": "off",
+    'import/prefer-default-export': 'off',
+    'react/jsx-curly-brace-presence': [
+      'error',
+      { props: 'always', children: 'never' },
+    ],
   },
 };
