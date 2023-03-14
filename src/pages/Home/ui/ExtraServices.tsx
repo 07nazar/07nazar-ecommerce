dc037f1c63717f6e96b64aaa27224f2d379ab05e
07nazar <ecsta3452@gmail.com>
2023-03-14 15:13:43 +0200

    eslint

--- src/pages/Home/ui/ExtraServices.tsx
+++ src/pages/Home/ui/ExtraServices.tsx
@@ -1,70 +1,62 @@
-import { FC } from "react";
-import { AiOutlineSearch, AiOutlineShop } from "react-icons/ai";
-import { GiCheckedShield } from "react-icons/gi";
-import { MdOutlineSend } from "react-icons/md";
+import { FC } from 'react'
+import { AiOutlineSearch, AiOutlineShop } from 'react-icons/ai'
+import { GiCheckedShield } from 'react-icons/gi'
+import { MdOutlineSend } from 'react-icons/md'
 
-import { AppLink } from "shared/ui/AppLink";
+import { AppLink } from 'shared/ui/AppLink'
 
-import customizeProducts from "../assets/customizeProducts.png";
-import fastAir from "../assets/fastAir.png";
-import hubs from "../assets/hubs.png";
-import inspection from "../assets/inspection.png";
+import customizeProducts from '../assets/customizeProducts.png'
+import fastAir from '../assets/fastAir.png'
+import hubs from '../assets/hubs.png'
+import inspection from '../assets/inspection.png'
 
 const services = [
   {
-    to: "/",
+    to: '/',
     img: hubs,
     icon: <AiOutlineSearch size={20} />,
-    title: "Source from Industry Hubs",
+    title: 'Source from Industry Hubs',
   },
   {
-    to: "/",
+    to: '/',
     img: customizeProducts,
     icon: <AiOutlineShop size={20} />,
-    title: "Customize Your Products",
+    title: 'Customize Your Products',
   },
   {
-    to: "/",
+    to: '/',
     img: fastAir,
     icon: <MdOutlineSend size={20} />,
-    title: "Fast, reliable shipping by ocean or air",
+    title: 'Fast, reliable shipping by ocean or air',
   },
   {
-    to: "/",
+    to: '/',
     img: inspection,
     icon: <GiCheckedShield size={20} />,
-    title: "Product monitoring and inspection",
+    title: 'Product monitoring and inspection',
   },
-];
+]
 
 export const ExtraServices: FC = () => (
   <>
-    <h3 className={"title mb-6"}>Our extra services</h3>
-    <div className={"flex gap-5"}>
+    <h3 className={'title mb-6'}>Our extra services</h3>
+    <div className={'flex gap-5'}>
       {services.map((service) => (
         <AppLink
           to={service.to}
           key={service.title}
-          className={"relative rounded-md h-52 bg-white items-start"}
-          textColor={"text-black hover:text-gray-dark"}
-        >
-          <img
-            src={service.img}
-            alt="service"
-            className={"w-full max-h-32 h-full bg-black"}
-          />
+          className={'relative rounded-md h-52 bg-white items-start'}
+          textColor={'text-black hover:text-gray-dark'}>
+          <img src={service.img} alt="service" className={'w-full max-h-32 h-full bg-black'} />
           <span
             className={
-              "absolute top-[6.25rem] right-5 rounded-full p-4 bg-lightblue border border-white"
-            }
-          >
+              'absolute top-[6.25rem] right-5 rounded-full p-4 bg-lightblue border border-white'
+            }>
             {service.icon}
           </span>
-          <p className={"my-4 mx-5 w-7/12 font-medium leading-[22px]"}>
-            {service.title}
-          </p>
+          <p className={'my-4 mx-5 w-7/12 font-medium leading-[22px]'}>{service.title}</p>
         </AppLink>
       ))}
     </div>
   </>
-);
+)
