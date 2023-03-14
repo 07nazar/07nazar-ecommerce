cb9416c48d715c9a45cc00d5c80310fee7a03bd8
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-03-14 10:14:19 +0300

    табы продукта

---
+++ src/pages/Product/ui/ProductContent.tsx
@@ -0,0 +1,100 @@
+import { FC, useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+import { ProductType } from "entities/ProductCard";
+import { SellerType } from "entities/UserCard";
+import { TabBtn, Tabs } from "shared/ui/Tabs";
+
+import { AboutCompany } from "./AboutCompany";
+import { Description } from "./Description";
+import { Reviews } from "./Reviews";
+import { Shipping } from "./Shipping";
+
+interface ProductContentProps {
+  product: ProductType;
+  seller: SellerType;
+}
+
+export const ProductContent: FC<ProductContentProps> = ({
+  product,
+  seller,
+}) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const [activeTab, setActiveTab] = useState<number>(
+    Number(new URLSearchParams(location.search).get("tab")) || 0
+  );
+
+  const detailProduct = [
+    {
+      buttonText: "Description",
+      content: (
+        <Description
+          description={product.description}
+          advantages={product.advantages}
+          specifications={product.specifications}
+        />
+      ),
+    },
+    {
+      buttonText: "Reviews",
+      content: <Reviews reviews={product.reviews} />,
+    },
+    {
+      buttonText: "Shipping",
+      content: (
+        <Shipping
+          cost={seller.delivery.cost}
+          regions={seller.delivery.regions}
+        />
+      ),
+    },
+    {
+      buttonText: "About company",
+      content: <AboutCompany {...seller} />,
+    },
+  ];
+
+  useEffect(() => {
+    const tabParam = new URLSearchParams(location.search).get("tab");
+    const tabParamIndex = detailProduct.findIndex(
+      (detail) => detail.buttonText.toLowerCase() === tabParam
+    );
+    if (tabParamIndex !== -1) {
+      setActiveTab(tabParamIndex);
+    }
+  }, [detailProduct, location.search]);
+
+  const handleTabClick = (index: number) => {
+    setActiveTab(index);
+    navigate(`?tab=${detailProduct[index].buttonText.toLowerCase()}`, {
+      replace: true,
+    });
+  };
+
+  return (
+    <Tabs
+      border={"border border-gray-medium bg-white"}
+      className={"pl-2"}
+      buttons={detailProduct.map((detail, i) => (
+        <TabBtn
+          isPill={false}
+          className={"pt-4 pb-3"}
+          outlined
+          key={detail.buttonText}
+          activeTab={activeTab}
+          index={i}
+          setActiveTab={handleTabClick}
+        >
+          {detail.buttonText}
+        </TabBtn>
+      ))}
+      orientation={"horizontal"}
+    >
+      <div className={"pt-2 pr-16 pb-7 pl-5"}>
+        {detailProduct[activeTab].content}
+      </div>
+    </Tabs>
+  );
+};
--- src/pages/Product/ui/index.tsx
+++ src/pages/Product/ui/index.tsx
@@ -1,111 +1,67 @@
-import { useState } from "react";
+import { FC } from 'react';
 
-import { TabBtn, Tabs } from "shared/ui/Tabs";
+import { ProductCard } from 'entities/ProductCard';
+import { DiscountBanner } from 'shared/ui/DiscountBanner';
 
-import { ProductCard } from "entities/ProductCard";
+import {
+  productFromServer,
+  productsLikes,
+  productsRelated,
+  sellerFromServer,
+} from '../model';
 
-import img from "../assets/phone.png";
-import { reviewsFromServer } from "../model";
+import { ProductContent } from './ProductContent';
 
-import { Description } from "./Description";
-import { Reviews } from "./Reviews";
-
-const detailProduct = [
-  {
-    buttonText: "Description",
-    content: (
-      <Description
-        specifications={[
-          { name: "Model", value: "#5236326" },
-          { name: "Style", value: "Classic" },
-          { name: "Type", value: "#5236326" },
-          { name: "Color", value: "Classic" },
-          { name: "Weight", value: "Classic" },
-        ]}
-        advantages={[
-          "Some great feature name here",
-          "Lorem ipsum dolor sit amet, consectetur",
-          "Duis aute irure dolor in reprehenderit",
-          "Some great feature name 2",
-        ]}
-        description={
-          "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, \n" +
-          "Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "
-        }
-      />
-    ),
-  },
-  {
-    buttonText: "Reviews",
-    content: <Reviews reviews={reviewsFromServer} />,
-  },
-  {
-    buttonText: "Shipping",
-    content: <p>3</p>,
-  },
-  {
-    buttonText: "About company",
-    content: <p>4</p>,
-  },
-];
-
-export const Product = () => {
-  const [activeTab, setActiveTab] = useState(0);
-
-  return (
-    <div>
-      <div>
-        <Tabs
-          border={"border border-gray-medium bg-white"}
-          className={"pl-2"}
-          buttons={detailProduct.map((detail, i) => (
-            <TabBtn
-              isPill={false}
-              className={"pt-4 pb-3"}
-              outlined
-              key={detail.buttonText}
-              activeTab={activeTab}
-              index={i}
-              setActiveTab={() => setActiveTab(i)}
-            >
-              {detail.buttonText}
-            </TabBtn>
+export const Product: FC = () => (
+  <>
+    <div className={'flex items-start gap-5 mb-5'}>
+      <ProductContent product={productFromServer} seller={sellerFromServer} />
+      <div
+        className={
+          'h-auto max-w-[280px] w-full bg-white border border-gray-medium rounded-md px-4 py-5'
+        }>
+        <h3 className={'mb-3 text-black font-semibold leading-5'}>
+          You may like
+        </h3>
+        <div className={'flex flex-col gap-4'}>
+          {productsLikes.map(liked => (
+            <ProductCard
+              key={liked.name}
+              product={liked}
+              className={{
+                image:
+                  'max-w-[80px] max-h-[80px] object-cover border border-gray-medium rounded-md',
+                title: 'max-w-[151px] w-full text-black text-base',
+                box: 'flex gap-2.5 max-w-[242px] w-full',
+                price: 'text-base text-gray-hot',
+                content: '',
+              }}
+            />
           ))}
-          orientation={"horizontal"}
-        >
-          <div className={"pt-2 pr-16 pb-7 pl-5"}>
-            {detailProduct[activeTab].content}
-          </div>
-        </Tabs>
-      </div>
-      <div className="grid grid-cols-6  gap-5 max-w[1180px] w-full bg-white mt-5 p-5">
-        <ProductCard
-          image={img}
-          price={{ current: 32 }}
-          title="Xiaomi Redmi 8 Original "
-          className={{
-            image: "w-[172px] bg-gray-medium rounded-md mb-3.5",
-            title: "max-w-[142px] w-full text-gray-dark",
-            box: "inline-flex flex-col",
-            price: "text-gray-hot text-base",
-            content: "",
-          }}
-        />
-      </div>
-      <div className="grid grid-cols-1 max-w[242px] gap-4 pl-4 pr-5 w-full bg-white">
-        <ProductCard
-          image={img}
-          price={{ current: 32 }}
-          title="Xiaomi Redmi 8 Original "
-          className={{
-            image: "w-[80px] border border-gray-medium rounded-md",
-            title: "max-w-[151px] w-full text-black text-base",
-            box: "flex gap-2.5",
-            price: "text-base text-gray-hot",
-            content: "",
-          }}
-        />
+        </div>
       </div>
     </div>
-  );
-};
+
+    <div className={'bg-white border border-gray-medium rounded-md p-5 mb-5'}>
+      <h3 className={'mb-4 text-black text-xl font-semibold'}>
+        Related products
+      </h3>
+      <div className={'flex gap-5'}>
+        {productsRelated.map(relate => (
+          <ProductCard
+            key={relate.name}
+            product={relate}
+            className={{
+              image: 'w-[172px] bg-gray-medium rounded-md mb-3.5',
+              title: 'max-w-[142px] w-full text-gray-dark',
+              box: 'inline-flex flex-col',
+              price: 'text-gray-hot text-base',
+              content: '',
+            }}
+          />
+        ))}
+      </div>
+    </div>
+    <DiscountBanner />
+  </>
+);
