c5fedb2db952a6565b87e43d59cbc049fe2a61ea
07nazar <ecsta3452@gmail.com>
2023-03-14 15:17:22 +0200

    Страница shop добавил сделаные блоки

--- src/pages/Shop/ui/index.tsx
+++ src/pages/Shop/ui/index.tsx
@@ -1,14 +1,47 @@
+import { IoMdClose } from 'react-icons/io'
+
+import { Breadcrumbs } from 'shared/ui/Breadcrumbs'
+import { Button } from 'shared/ui/Button'
+
+import { ConrolPanel } from './ConrolPanel'
+import { PaginationBox } from './PaginationBox'
 import { ProductCol } from './ProductCol'
 import { ProductRow } from './ProductRow'
 
+const items = [
+  { text: 'text1' },
+  { text: 'text2' },
+  { text: 'text3' },
+  { text: 'text4' },
+  { text: 'text5' },
+  { text: 'text6' },
+]
+
 export const Shop = () => (
   <div>
+    <Breadcrumbs />
+
+    <ConrolPanel category="Mobile accessory" itemsCount={12199} />
+
+    <div className="flex gap-2">
+      {items.map(({ text }) => (
+        <Button
+          className="flex items-center p-1.5 gap-2.5 border border-blue bg-white text-gray-dark"
+          key={text}>
+          {text}
+          <IoMdClose size={18} className="text-gray-hot" />
+        </Button>
+      ))}
+      <Button className="text-blue rounded-none">Clear all filter</Button>
+    </div>
+
     <div className="max-w-[926px] w-full grid grid-cols-3 gap-5 bg-white">
       <ProductCol />
     </div>
-
     <div className="grid grid-cols-1 gap-2.5 max-w[920px] w-full">
       <ProductRow />
     </div>
+
+    <PaginationBox />
   </div>
 )
