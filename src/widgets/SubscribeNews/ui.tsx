3e31044a059d98b4e33ea4a80a106c461b3b8775
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-03-20 20:48:57 +0300

    адаптив

--- src/widgets/SubscribeNews/ui.tsx
+++ src/widgets/SubscribeNews/ui.tsx
@@ -13,19 +13,19 @@ export const SubscribeNews = () => {
   };
 
   return (
-    <div className={'relative py-10'}>
+    <div className={'relative py-10 md:py-5'}>
       <span
         className={'-z-10 absolute inset-0 full-screen w-screen bg-gray-pale'}
       />
       <div className={'flex flex-col items-center'}>
-        <h3 className={'text-black text-xl font-semibold'}>
+        <h3 className={'text-black text-xl font-semibold text-center'}>
           Subscribe on our newsletter
         </h3>
-        <h5 className={'text-gray-hot'}>
+        <h5 className={'mb-5 sm:mb-2 text-gray-hot text-center'}>
           Get daily news on upcoming offers from many suppliers all over the
           world
         </h5>
-        <div className={'flex gap-2'}>
+        <div className={'flex gap-2 md:flex-wrap md:justify-center'}>
           <Input
             value={email}
             onChange={e => setEmail(e.target.value)}
