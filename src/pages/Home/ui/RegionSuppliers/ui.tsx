e8648d67c2342ea78cbf608d4ec0504bb0c7e4e6
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-03-23 16:21:17 +0300

    адаптив

--- src/pages/Home/ui/RegionSuppliers/ui.tsx
+++ src/pages/Home/ui/RegionSuppliers/ui.tsx
@@ -1,91 +1,41 @@
 import { FC } from 'react';
+import { FreeMode, Grid } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/grid';
 
 import { AppLink } from 'shared/ui/AppLink';
 
-import arabicFlag from '../../assets/arabic.png';
-import australiaFlag from '../../assets/australia.png';
-import britainFlag from '../../assets/britain.png';
-import chinaFlag from '../../assets/china.png';
-import denmarkFlag from '../../assets/denmark.png';
-import franceFlag from '../../assets/france.png';
-import germanyFlag from '../../assets/germany.png';
-import italyFlag from '../../assets/italy.png';
-import usaFlag from '../../assets/usa.png';
-
-interface Country {
-  name: string;
-  site: string;
-  flag: string;
-}
-const countries: Country[] = [
-  {
-    name: 'Arabic Emerates',
-    site: 'shopname.ae',
-    flag: arabicFlag,
-  },
-  {
-    name: 'Australia',
-    site: 'shopname.ae',
-    flag: australiaFlag,
-  },
-  {
-    name: 'United States',
-    site: 'shopname.com',
-    flag: usaFlag,
-  },
-  {
-    name: 'Germany',
-    site: 'shopname.de',
-    flag: germanyFlag,
-  },
-  {
-    name: 'Italy',
-    site: 'shopname.ae',
-    flag: italyFlag,
-  },
-  {
-    name: 'Denmark',
-    site: 'shopname.com.dk',
-    flag: denmarkFlag,
-  },
-  {
-    name: 'France',
-    site: 'shopname.com.fr',
-    flag: franceFlag,
-  },
-  {
-    name: 'Arabic',
-    site: 'shopname.ae',
-    flag: arabicFlag,
-  },
-  {
-    name: 'China',
-    site: 'shopname.ae',
-    flag: chinaFlag,
-  },
-  {
-    name: 'Great Britain',
-    site: 'shopname.co.uk',
-    flag: britainFlag,
-  },
-];
+import { countries } from './model';
 
 export const RegionSuppliers: FC = () => (
   <>
-    <h3 className={'title mb-6'}>Suppliers by region</h3>
-    <div className={'grid gap-2.5 grid-cols-5'}>
+    <h3 className={'title mb-6 sm:text-center z-10'}>Suppliers by region</h3>
+    <Swiper
+      slidesPerView={'auto'}
+      modules={[Grid, FreeMode]}
+      freeMode
+      grid={{
+        rows: 2,
+        fill: 'row',
+      }}>
       {countries.map(country => (
-        <AppLink
+        <SwiperSlide
           key={country.name}
-          to={`https://${country.site}`}
-          className={'flex-row gap-2.5 items-center'}>
-          <img src={country.flag} alt={country.flag} />
-          <div>
-            <p className={'text-black'}>{country.name}</p>
-            <p className={'text-xs text-gray-hot'}>{country.site}</p>
-          </div>
-        </AppLink>
+          className={'max-w-[255px] md:max-w-[170px]'}>
+          <AppLink
+            to={`https://${country.site}`}
+            className={'w-fit flex-row gap-2.5 lg:gap-1.5 items-center'}>
+            <img src={country.flag} alt={country.flag} />
+            <div>
+              <p className={'text-black'}>{country.name}</p>
+              <p className={'text-xs text-gray-hot'}>{country.site}</p>
+            </div>
+          </AppLink>
+        </SwiperSlide>
       ))}
-    </div>
+    </Swiper>
   </>
 );
