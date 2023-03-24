0eddc306c0c53011c0d945f01ca71bae44657cee
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-03-24 13:54:32 +0300

    адаптив для блока рек. товаров

--- src/pages/Home/ui/Recommendation/ui.tsx
+++ src/pages/Home/ui/Recommendation/ui.tsx
@@ -1,22 +1,33 @@
 import { FC } from 'react';
 
 import { ProductRecommended } from 'widgets/ProductCard';
+import { useMatchMedia } from 'shared/lib';
 
 import { products } from '../../model/data';
 
-export const RecommendedItems: FC = () => (
-  <div className={'mb-8'}>
-    <h3 className={'title mb-6'}>Recommended items</h3>
-    <div className={'grid grid-cols-5 gap-5'}>
-      {products.map(product => (
-        <ProductRecommended
-          id={product.id}
-          key={product.name}
-          name={product.name}
-          price={product.price}
-          mainPhoto={product.mainPhoto}
-        />
-      ))}
+export const RecommendedItems: FC = () => {
+  const { isTablet } = useMatchMedia();
+
+  const maxProductsCount = isTablet ? 6 : 10;
+  const productsToShow = products.slice(0, maxProductsCount);
+
+  return (
+    <div className={'mb-8'}>
+      <h3 className={'title mb-6 md:mb-4 sm:mb-2.5'}>Recommended items</h3>
+      <div
+        className={
+          'grid grid-cols-5 md:grid-cols-3 sm:grid-cols-2 gap-5 lg:gap-2 sm:mx-2.5'
+        }>
+        {productsToShow.map(product => (
+          <ProductRecommended
+            id={product.id}
+            key={product.name}
+            name={product.name}
+            price={product.price}
+            mainPhoto={product.mainPhoto}
+          />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
