220eaaed9ef05d520a8cb0bb0e7365d09c27ea01
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-03-26 21:00:49 +0300

    адаптив формы оформления заказа

--- src/pages/Cart/ui/CheckoutForm/ui.tsx
+++ src/pages/Cart/ui/CheckoutForm/ui.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 
 import { CartType } from 'entities/Cart';
+import { useMatchMedia } from 'shared/lib';
 import { Button } from 'shared/ui/Button';
 
 import payment1 from '../../assets/payment-1.png';
@@ -9,47 +10,69 @@ import payment3 from '../../assets/payment-3.png';
 import payment4 from '../../assets/payment-4.png';
 import payment from '../../assets/payment.png';
 
-type CheckoutProps = Pick<CartType, 'subtotal' | 'discount' | 'tax' | 'total'>;
+type CheckoutProps = Pick<
+  CartType,
+  'subtotal' | 'discount' | 'tax' | 'total'
+> & {
+  countItems: number;
+};
 
 export const CheckoutForm: FC<CheckoutProps> = ({
   subtotal,
   discount,
   tax,
   total,
-}) => (
-  <div className={'base-border-gray'}>
-    <div className={'flex flex-col gap-1 text-gray-dark px-1 mb-4'}>
-      <p className={' flex justify-between  '}>
-        Subtotal <span>${subtotal.toFixed(2)}</span>
-      </p>
-      <p className={' flex justify-between'}>
-        Discount <span className={'text-red'}>- ${discount.toFixed(2)}</span>
-      </p>
-      <p className={' flex justify-between '}>
-        Tax <span className={'text-green'}>+ ${tax.toFixed(2)}</span>
-      </p>
-    </div>
-    <p
-      className={
-        'text-black font-semibold pt-2.5 border-t border-gray-medium px-1 flex justify-between mb-5'
-      }>
-      Total <span className={'text-xl'}>${total.toFixed(2)}</span>
-    </p>
+  countItems,
+}) => {
+  const { isMobile } = useMatchMedia();
 
-    <Button
-      className={
-        'mb-4 bg-green hover:brightness-125 duration-300 w-full justify-center'
-      }
-      size={'lg'}>
-      Checkout
-    </Button>
+  // Todo +- должны замениться на $
 
-    <div className={'flex gap-2 justify-center'}>
-      <img src={payment} alt={'payment method'} />
-      <img src={payment1} alt={'payment method'} />
-      <img src={payment2} alt={'payment method'} />
-      <img src={payment3} alt={'payment method'} />
-      <img src={payment4} alt={'payment method'} />
+  return (
+    <div className={'base-border-gray sm:border-y sm:rounded-none'}>
+      <div className={'flex flex-col gap-1 text-gray-dark px-1 mb-4 md:mb-2'}>
+        <p className={'flex justify-between'}>
+          Subtotal{' '}
+          <span className={'sm:font-medium'}>${subtotal.toFixed(2)}</span>
+        </p>
+        <p className={'flex justify-between'}>
+          Discount
+          <span className={'text-red sm:font-medium sm:text-black'}>
+            - ${discount.toFixed(2)}
+          </span>
+        </p>
+        <p className={'flex justify-between'}>
+          Tax
+          <span className={'text-green sm:font-medium sm:text-black'}>
+            + ${tax.toFixed(2)}
+          </span>
+        </p>
+      </div>
+      <p
+        className={
+          'text-black font-semibold pt-2.5 sm:pt-0 border-t sm:border-none border-gray-medium px-1 flex justify-between mb-5'
+        }>
+        Total <span className={'text-xl'}>${total.toFixed(2)}</span>
+      </p>
+
+      <Button
+        className={
+          'mb-4 sm:mb-0 bg-green hover:brightness-125 w-full justify-center'
+        }
+        size={isMobile ? 'md' : 'lg'}>
+        Checkout
+        {isMobile && <span className={'ml-1'}>({countItems} items)</span>}
+      </Button>
+
+      {!isMobile && (
+        <div className={'flex gap-2 justify-center'}>
+          <img src={payment} alt={'payment method'} />
+          <img src={payment1} alt={'payment method'} />
+          <img src={payment2} alt={'payment method'} />
+          <img src={payment3} alt={'payment method'} />
+          <img src={payment4} alt={'payment method'} />
+        </div>
+      )}
     </div>
-  </div>
-);
+  );
+};
