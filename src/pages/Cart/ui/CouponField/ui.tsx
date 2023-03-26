0cb5a0921233397cea6d562e692d4913420aa4ff
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-03-26 21:00:28 +0300

    адаптив блока с купоном

--- src/pages/Cart/ui/CouponField/ui.tsx
+++ src/pages/Cart/ui/CouponField/ui.tsx
@@ -4,8 +4,10 @@ import { ApplyCoupon } from 'features/ApplyCoupon';
 import { Input, InputGroup } from 'shared/ui/Input';
 
 export const CouponField: FC = () => (
-  <div className={'base-border-gray'}>
-    <h6 className={'text-gray-dark leading-5 mb-2.5'}>Have a coupon?</h6>
+  <div className={'base-border-gray sm:border-none'}>
+    <h6 className={'text-gray-dark leading-5 mb-2.5 sm:text-center'}>
+      Have a coupon?
+    </h6>
     <InputGroup>
       <Input
         className={'h-full border border-gray-medium border-r-0'}
