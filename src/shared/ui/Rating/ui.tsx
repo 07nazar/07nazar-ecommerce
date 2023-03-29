5fe9575c2c1db177f41bcce58a1135f44abb0a36
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-03-29 14:48:49 +0300

    вывод числа рейтинга

--- src/shared/ui/Rating/ui.tsx
+++ src/shared/ui/Rating/ui.tsx
@@ -3,20 +3,24 @@ import { GoStar } from 'react-icons/go';
 
 interface RatingProps {
   value: number;
+  showValue?: boolean;
 }
 
-export const Rating: FC<RatingProps> = ({ value }) => {
+export const Rating: FC<RatingProps> = ({ value, showValue }) => {
   const filledStars = Math.round(value);
   const emptyStars = 5 - filledStars;
 
   return (
-    <>
+    <div className={'flex items-center '}>
       {[...Array(filledStars)].map((_, index) => (
         <GoStar key={index + 5} className={'text-orange'} />
       ))}
       {[...Array(emptyStars)].map((_, index) => (
         <GoStar key={index + 5} className={'text-gray-medium'} />
       ))}
-    </>
+      {showValue && (
+        <span className={'ml-1 text-orange'}>{value.toFixed(1)}</span>
+      )}
+    </div>
   );
 };
