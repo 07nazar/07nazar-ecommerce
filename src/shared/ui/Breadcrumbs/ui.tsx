dc9a1b4bd323f544788221b5242d14cbdf338c12
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-03-29 14:52:55 +0300

    eslint fix

--- src/shared/ui/Breadcrumbs/ui.tsx
+++ src/shared/ui/Breadcrumbs/ui.tsx
@@ -6,25 +6,25 @@ export const Breadcrumbs = () => {
   const pathnames = location.pathname.split('/').filter(x => x);
 
   return (
-    <nav className='py-5'>
-      <ul className='flex items-center gap-1'>
-        <li className='flex items-center'>
-          <Link to='/' className='text-gray-hot'>
+    <nav className={'py-5'}>
+      <ul className={'flex items-center gap-1'}>
+        <li className={'flex items-center'}>
+          <Link to={'/'} className={'text-gray-hot'}>
             Home
           </Link>
-          <RiArrowRightSLine color='#8B96A5' width={6} height={10} />
+          <RiArrowRightSLine color={'#8B96A5'} width={6} height={10} />
         </li>
         {pathnames.map((name, index) => {
           const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`;
           return (
-            <li key={routeTo} className='flex items-center text-gray-hot'>
+            <li key={routeTo} className={'flex items-center text-gray-hot'}>
               <Link
                 to={routeTo}
-                className='text-gray-hot first-letter:uppercase'>
+                className={'text-gray-hot first-letter:uppercase'}>
                 {name}
               </Link>
               {index !== pathnames.length - 1 && (
-                <RiArrowRightSLine color='#8B96A5' width={6} height={10} />
+                <RiArrowRightSLine color={'#8B96A5'} width={6} height={10} />
               )}
             </li>
           );
