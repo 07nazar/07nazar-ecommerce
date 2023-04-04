32ecd34d7e71626e7ac48c416ba117c80ea7acc6
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-04-04 20:53:05 +0300

    добавил productId в пропсы, убрал картинку только на телефоне

--- src/features/MoveToCart/ui.tsx
+++ src/features/MoveToCart/ui.tsx
@@ -5,18 +5,19 @@ import { useMatchMedia } from 'shared/lib';
 import { Button } from 'shared/ui/Button';
 
 interface IMoveToCartProps {
+  productId: number;
   className?: string;
 }
 
-export const MoveToCart: FC<IMoveToCartProps> = ({ className }) => {
-  const { isDesktop } = useMatchMedia();
+export const MoveToCart: FC<IMoveToCartProps> = ({ className, productId }) => {
+  const { isMobile } = useMatchMedia();
 
   return (
     <Button
       className={`${
         className || ''
       } flex border rounded-md border-gray-medium text-blue sm:text-xs md:p-2 sm:p-1.5 gap-1 hover:bg-blue hover:text-white duration-300`}>
-      {isDesktop && <IoMdCart size={18} />} Move to cart
+      {!isMobile && <IoMdCart size={18} />} Move to cart
     </Button>
   );
 };
