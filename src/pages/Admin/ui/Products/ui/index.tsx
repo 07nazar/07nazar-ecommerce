a0b8f653b778c97ca880e724333f019133d4e310
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-04-21 15:17:55 +0300

    разбил вывод продуктов в отдельной компонент

---
+++ src/pages/Admin/ui/Products/ui/index.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+import { Search } from 'shared/ui/Search';
+
+import { SearchResult } from './SearchResult';
+
+export const Products = () => {
+  const [searchValue, setSearchValue] = useState<string>('');
+
+  return (
+    <div className={'flex flex-col w-full'}>
+      <Search
+        handleChange={value => {
+          setSearchValue(value);
+        }}
+        className={
+          'mb-2 w-full bg-white border border-lightblue h-10 rounded-md'
+        }
+      />
+      <SearchResult value={searchValue} />
+    </div>
+  );
+};
