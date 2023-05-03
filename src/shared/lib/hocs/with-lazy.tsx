36c1233ba7b8bb4ce95a605fda70d02caac7d7ae
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-05-03 17:55:04 +0300

    выровнил по центру loader + заменил JSX на ReactNode, для реюзабельности

--- src/shared/lib/hocs/with-lazy.tsx
+++ src/shared/lib/hocs/with-lazy.tsx
@@ -1,7 +1,9 @@
-import { Suspense } from 'react';
+import { ReactNode, Suspense } from 'react';
 
 import { Loader } from '../../ui/loader';
 
-export const withLazy = (component: JSX.Element) => (
-  <Suspense fallback={<Loader />}>{component}</Suspense>
+export const withLazy = (component: ReactNode) => (
+  <Suspense fallback={<Loader className={'mx-auto my-5'} />}>
+    {component}
+  </Suspense>
 );
