ee337e253f97c7c403f50a3ea6703a0ae9350ed3
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-05-03 13:24:13 +0300

    тест чанков на продашкн

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,33 @@ import tailwindcss from 'tailwindcss';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 
+import path from 'path';
+import * as fs from 'fs';
+
+const mainDeps = {
+  main: [
+    'react',
+    'react-router-dom',
+    'react-dom',
+    '@reduxjs/toolkit',
+    '@reduxjs/toolkit/query/react',
+  ],
+  common: ['swiper', 'swiper/react', '@react-spring/web'],
+};
+function renderChunks() {
+  const chunks: Record<string, string[]> = {};
+
+  const pagesDir = path.resolve(__dirname, 'src/pages');
+  const pages = fs
+    .readdirSync(pagesDir)
+    .filter(p => fs.statSync(path.join(pagesDir, p)).isDirectory());
+
+  pages.forEach(page => {
+    chunks[page] = [path.join(pagesDir, page)];
+  });
+
+  return chunks;
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -13,13 +40,6 @@ export default defineConfig({
     tsconfigPaths(),
     tailwindcss(),
     svgr(),
-    visualizer({
-      template: 'treemap',
-      open: false,
-      gzipSize: true,
-      brotliSize: true,
-      filename: 'analyst.html',
-    }) as PluginOption,
     viteImagemin({
       gifsicle: {
         optimizationLevel: 7,
@@ -47,5 +67,23 @@ export default defineConfig({
         ],
       },
     }),
+    visualizer({
+      template: 'treemap',
+      open: false,
+      gzipSize: true,
+      brotliSize: true,
+      filename: 'analyst.html',
+    }) as PluginOption,
   ],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          ...mainDeps,
+          ...renderChunks(),
+        },
+      },
+    },
+  },
 });
