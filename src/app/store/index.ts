47ad3e5a12f3bbeb72a6ce2180a573df5d29d571
07nazar <ecsta3452@gmail.com>
2023-05-05 14:54:32 +0300

    Добавил invalidateAccessTokenListener middleware

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query/react';
 
+import { invalidateAccessTokenListener } from 'features/authentication/InvalidateAccessToken';
 import { baseApi } from 'shared/api/baseApi';
 import { isDevEnv } from 'shared/config';
 
@@ -9,7 +10,10 @@ import { rootReducer } from './rootReducer';
 export const store = configureStore({
   reducer: rootReducer,
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(baseApi.middleware),
+    getDefaultMiddleware().concat(
+      baseApi.middleware,
+      invalidateAccessTokenListener.middleware
+    ),
   devTools: isDevEnv,
 });
 
