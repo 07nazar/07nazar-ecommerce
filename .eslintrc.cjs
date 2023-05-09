13069e84c39223ca3e19d5e5857682044dfdba5a
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-05-09 23:17:05 +0300

    обновление плагина conarti-fsd, с фиксом импортов

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,3 @@
-const FS_LAYERS = ['app', 'processes', 'pages', 'widgets', 'features', 'entities', 'shared']
-
 module.exports = {
   root: true,
   env: {
@@ -43,26 +41,6 @@ module.exports = {
     ],
     'conarti-fsd/absolute-relative': 'error',
     'conarti-fsd/public-api': 'error',
-    'import/order': [
-      2,
-      {
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-        'newlines-between': 'always',
-        pathGroups: FS_LAYERS.map(
-          (layer) => ({
-            pattern: `**/?(*)${layer}{,/**}`,
-            group: 'internal',
-            position: 'after',
-          }),
-        ),
-        distinctGroup: false,
-        pathGroupsExcludedImportTypes: ['builtin', 'type'],
-        groups: ['builtin', 'external', 'internal', 'type', 'parent', 'sibling', 'index'],
-      },
-    ],
     'react/react-in-jsx-scope': 'off',
     'react/jsx-uses-react': 'off',
     'react-hooks/exhaustive-deps': 'off',
