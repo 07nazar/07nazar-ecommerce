66cb6de7f39ca7d927afae19400c228b520059ab
Kirill <pavlovkirill322@gmail.com>
2023-06-13 15:05:41 +0300

    удалил ненужный тип

--- src/entities/product/types.ts
+++ src/entities/product/types.ts
@@ -46,8 +46,3 @@ export type ProductRow = Pick<
 >;
 
 export type ProductMin = Pick<Product, 'id' | 'name' | 'mainPhoto' | 'price'>;
-
-export type ProductTableAdmin = Pick<
-  Product,
-  'id' | 'name' | 'mainPhoto' | 'price' | 'sellerId'
->;
